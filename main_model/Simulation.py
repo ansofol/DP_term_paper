e38a9cad88c06f58eed6d2e0b45ac4f48bc77e9e
rvb248 <86844330+rvb248@users.noreply.github.com>
2023-05-11 14:30:54 +0200

    Kunne godt trænge til et gennemsyn :(

--- main_model/Simulation.py
+++ main_model/Simulation.py
@@ -8,49 +8,54 @@ from DC_EGM import value_of_choice_study
 def simulate(sim,sol,par): 
 
     #Initialize 
-    sim.a[:,:,0] = par.a_initial
-    
-    u_choice = np.random.rand(par.Ntypes,par.N,par.Smax) # Used for education choices 
-    
-    wage_shock = np.random.choice(par.neps,(par.Ntypes,par.N,par.Tsim),replace=True,p = par.eps_w/np.sum(par.eps_w))
+    sim.m[:,0] = par.m_initial
+
+    random = par.random
+
+
+    sim.type = random.choice(par.Ntypes,par.N,replace=True,p=par.dist)
+
+    u_choice = random.rand(par.N,par.Smax) # Used for education choices 
+
+    wage_shock = random.choice(par.neps,(par.N,par.Tsim),replace=True,p = par.eps_w/np.sum(par.eps_w))
 
     
     for t in range(par.Tsim): 
-        for z in range(par.Ntypes):
-            for i in range(par.N):
-                shock = wage_shock[z,i,t]
-                choice = 0
+        for i in range(par.N):
+            shock = wage_shock[i,t]
+            choice = 0
+            type = sim.type[i]
 
-                if t <= par.Smax -1: # Periodes involving discrete decision for students
-                    # Find choices for people who study 
-                    if sim.S[z,i,t] == t: 
-                        
-                        m_study = sim.a[z,i,t] + transfer(z,par)
-                        V_study = tools.interp_linear_1d_scalar(sol.m[z,t,0,t,:,0], sol.V[z,t,0,t,:,0], m_study)
+            if t <= par.Smax -1: # Periodes involving discrete decision for students
+                # Find choices for people who study 
+                if sim.S[i,t] == t: 
+                    
+                    m_study = sim.m[i,t] + transfer(type,par)
+                    V_study = tools.interp_linear_1d_scalar(sol.m[type,t,0,t,:,0], sol.V[type,t,0,t,:,0], m_study)
 
-                        m_work =sim.a[z,i,t] 
-                        V_work = tools.interp_linear_1d_scalar(sol.m[z,t,1,t,par.Ba:,shock], sol.V[z,t,1,t,par.Ba:,shock], m_work)
+                    m_work =sim.m[i,t] 
+                    V_work = tools.interp_linear_1d_scalar(sol.m[type,t,1,t,par.Ba:,shock], sol.V[type,t,1,t,par.Ba:,shock], m_work)
 
-                        V = np.array([V_study,V_work])
+                    V = np.array([V_study,V_work])
 
-                        p = ccp(V,par)
+                    p = ccp(V,par)
 
-                        choice = u_choice[z,i,t] < 1 - p # Choice to continue to study
-                        sim.S[z,i,t+1] += sim.S[z,i,t] + choice # Change Education status
+                    choice = u_choice[i,t] < 1 - p # Choice to continue to study
+                    sim.S[i,t+1] += sim.S[i,t] + choice # Change Education status
 
-                    # Find Income and consumption dependent on choice 
-                if sim.S[z,i,t] == t and t < par.Smax and choice == 1 : # People who continue to study 
-                    income = sim.a[z,i,t] + transfer(z,par)
-                    sim.c[z,i,t] = tools.interp_linear_1d_scalar(sol.m[z,t,0,t,:,0], sol.c[z,t,0,t,:,0], sim.a[z,i,t] + transfer(z,par))
-                    sim.a[z,i,t+1] = (1+par.r)*(income - sim.c[z,i,t])
+                # Find Income and consumption dependent on choice 
+            if sim.S[i,t] == t and t < par.Smax and choice == 1 : # People who continue to study 
+                income = sim.m[i,t] + transfer(type,par)
+                sim.c[i,t] = tools.interp_linear_1d_scalar(sol.m[type,t,0,t,:,0], sol.c[type,t,0,t,:,0], sim.m[i,t] + transfer(type,par))
+                sim.m[i,t+1] = (1+par.r)*(income - sim.c[i,t])
 
-                else:  # People who work  
-                    s = int(np.max(sim.S[z,i,:],axis=0))
-                    sim.c[z,i,t] = tools.interp_linear_1d_scalar(sol.m[z,t,1,s,par.Ba:,shock], sol.c[z,t,1,s,par.Ba:,shock], sim.a[z,i,t])
-                    sim.ell[z,i,t] = tools.interp_linear_1d_scalar(sol.m[z,t,1,s,par.Ba:,shock], sol.ell[z,t,1,s,par.Ba:,shock], sim.a[z,i,t])
-                    income = sim.a[z,i,t] + wage_func(s,t,z,par.eps_grid[shock],par)*sim.ell[z,i,t]
-                    if t < par.Tmax - 1:
-                        sim.a[z,i,t+1] = (1+par.r)*(income - sim.c[z,i,t])
+            else:  # People who work  
+                s = int(np.max(sim.S[i,:],axis=0))
+                sim.c[i,t] = tools.interp_linear_1d_scalar(sol.m[type,t,1,s,par.Ba:,shock], sol.c[type,t,1,s,par.Ba:,shock], sim.m[i,t])
+                sim.ell[i,t] = tools.interp_linear_1d_scalar(sol.m[type,t,1,s,par.Ba:,shock], sol.ell[type,t,1,s,par.Ba:,shock], sim.m[i,t])
+                income = sim.m[i,t] + wage_func(s,t,type,par.eps_grid[shock],par)*sim.ell[i,t]
+                if t < par.Tmax - 1:
+                    sim.m[i,t+1] = (1+par.r)*(income - sim.c[i,t])
                         
 
 def ccp(V,par): 
