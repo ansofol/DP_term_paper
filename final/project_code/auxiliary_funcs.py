9afba638e0e5faeac732b55c1c3fec2c916b939c
ansofol <ansofol98@gmail.com>
2023-06-02 13:58:05 +0200

    docs for auxiliary_funcs.py

--- final/project_code/auxiliary_funcs.py
+++ final/project_code/auxiliary_funcs.py
@@ -1,4 +1,18 @@
 import numpy as np
 
 def wage_func(i_S, t, i_type, eta, par):
+        """
+        Calculates the wage based on the given input parameters.
+
+        Parameters:
+        i_S (int): Education index.
+        t (float): Time period.
+        i_type (int):Type index.
+        eta (float): Wage shock.
+        par (object): An object containing necessary parameters for the calculation.
+
+        Returns:
+        float: The calculated wage based on the input parameters.
+
+        """
         return np.exp(par.lambda_vec[i_S]*np.log(1+par.theta[i_type]) + eta)
